2b806fe5848988532556e44e21f375b0225fff0d
frol1414 <frol1414@gmail.com>
2020-04-12 12:51:17 +0300

    add new fiches

---
+++ src/components/Navigation/MenuToggle/MenuToogle.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classes from './MenuToogle.css';
+
+const MenuToogle = props => {
+  const cls = [
+    classes.MenuToogle,
+    'fa',
+  ];
+  
+  if(props.isOpen) {
+    cls.push('fa-times');
+    cls.push(classes.open);
+  } else {
+    cls.push('fa-bars')
+  }
+  
+  return (
+    <i
+      className={cls.join(' ')}
+      onClick={props.onToggle}
+    />
+  )
+};
+
+export default MenuToogle
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,39 @@
 import React, {Component} from "react";
+import classes from './Layout.css';
+import MenuToogle from "../../components/Navigation/MenuToggle/MenuToogle";
+import Drawer from "../../components/Navigation/Drawer/Drawer";
 
 class Layout extends Component {
+  state = {
+    menu: false
+  }
+  
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu
+    })
+  };
+  
+  menuCloseHandler = () => {
+    this.setState({
+      menu:false
+    })
+  };
+  
   render() {
     return (
-      <div>
+      <div className={classes.Layout}>
+        
+        <Drawer
+          isOpen={this.state.menu}
+          onClose={this.menuCloseHandler}
+        />
+        
+        <MenuToogle
+        onToggle={this.toggleMenuHandler}
+        isOpen={this.state.menu}
+        />
+        
         <main>
           {this.props.children}
         </main>
