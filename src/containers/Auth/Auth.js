ece2e53fe4668684f48a4e0496cab788567d8228
frol1414 <frol1414@gmail.com>
2020-04-13 08:46:46 +0300

    add validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,8 +2,43 @@ import React, {Component} from "react";
 import classes from './Auth.css';
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
+import is from 'is_js'
 
+// function validateEmail(email) {
+//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// }
 export default class Auth extends Component {
+  
+  state = {
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  };
   
   loginHandler = () => {
   
@@ -17,6 +52,63 @@ export default class Auth extends Component {
     event.preventDefault()
   };
   
+  validateControl(value, validation) {
+    if(!validation) {
+      return true
+    }
+    
+    let isValid = true;
+    
+    if(validation.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+    
+    if(validation.email) {
+      isValid = is.email(value) && isValid
+    }
+    
+    if(validation.minLength) {
+      isValid = value.length >=validation.minLength && isValid
+    }
+    
+    return isValid
+  }
+  
+  onChangeHandler = (event, controlName) => {
+    const formControls = {...this.state.formControls};
+    const control = { ...formControls[controlName] };
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+    formControls[controlName] = control;
+    let isFormValid = true;
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid
+    })
+    this.setState({
+      formControls, isFormValid
+    })
+  };
+  
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={(event) => this.onChangeHandler(event, controlName)}
+        />
+      )
+    })
+  }
+  
   render() {
     return (
       <div className={classes.Auth}>
@@ -24,17 +116,16 @@ export default class Auth extends Component {
           <h1>Авторизация</h1>
           
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <Input
-              label="Email"
-            />
+            {this.renderInputs()}
             
-            <Input
-              label="Пароль"
-              errorMessage="test"
-            />
-            
-            <Button type="success" onClick={this.loginHandler}>Войти</Button>
-            <Button type="primary" onClick={this.registerHandler}>Зарегистрироваться</Button>
+            <Button type="success"
+                    onClick={this.loginHandler}
+                    disabled={!this.state.isFormValid}
+            >Войти</Button>
+            <Button type="primary"
+                    onClick={this.registerHandler}
+                    disabled={!this.state.isFormValid}
+            >Зарегистрироваться</Button>
           </form>
         </div>
       </div>
